4497dcf50c6f5affc7fc5c52b537ac8de45bc6ae
Alexandre Clark <alex.clark.015@gmail.com>
2018-11-12 19:29:07 -0500

    Progdyn (1 and 2)

--- src/main/java/com/inf4705/tp2/algorithms/ProgDyn1DynamiteMinimizer.java
+++ src/main/java/com/inf4705/tp2/algorithms/ProgDyn1DynamiteMinimizer.java
@@ -8,6 +8,52 @@ import com.inf4705.tp2.model.Dynamite;
 public class ProgDyn1DynamiteMinimizer extends BaseDynamiteMinimizer {
 	@Override
 	public List<Dynamite> minimizeDynamiteUsage(List<Dynamite> dynamites, int goal) {
+		// Step 1: Define c[1...N] ArrayList -> [0, N + 1]
+		// N : goal
+		// c[j] : minimum amount of dynamite required for the quantity
+		ArrayList<Double> countList = new ArrayList<Double>(goal + 1);
+
+		// Step 2: Define I[1...N] index ArrayList -> [0, N + 1]
+		ArrayList<Double> indexList = new ArrayList<Double>(goal + 1);
+
+		// Step 3: Fill count list with Integer.MAX_VALUE (suppose to be infinity)
+		for (int j = 0; j < goal + 1; j++) {
+			countList.add(j, Double.POSITIVE_INFINITY);
+			indexList.add(j, Double.POSITIVE_INFINITY);
+		}
+
+		// Step 4: Set boundaries values
+		// c[di] = 1
+		// I[di] = -i
+		for (int i = 1; i < dynamites.size(); i++) {
+			Integer dynamiteValue = dynamites.get(i).getPower();
+
+			if (dynamiteValue <= goal) {
+				countList.set(dynamiteValue, 1.0);
+				indexList.set(dynamiteValue, (double)(-i));
+			}
+		}
+
+		// Step 5: Main loop
+		for (int j = 1; j < countList.size(); j++) {
+			// Check if boundary
+			if (countList.get(j) != 1) {
+				for (int i = 1; i < Math.floor(j / 2); i++) {
+					if (countList.get(j) > countList.get(i) + countList.get(j - i)) {
+						countList.set(j, countList.get(i) + countList.get(j - i));
+						indexList.set(j, (double)(i));
+					}
+				}
+			}
+		}
+
+		System.out.printf("\n");
+		System.out.printf("Minimum : \n");
+		System.out.print(countList.get(goal).intValue());
+		System.out.printf("\nIndex : \n");
+		System.out.print(indexList.get(goal).intValue());
+		System.out.printf("\n");
+
 		return new ArrayList<>();
 	}
 }
--- src/main/java/com/inf4705/tp2/algorithms/ProgDyn2DynamiteMinimizer.java
+++ src/main/java/com/inf4705/tp2/algorithms/ProgDyn2DynamiteMinimizer.java
@@ -1,6 +1,7 @@
 package com.inf4705.tp2.algorithms;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import com.inf4705.tp2.model.Dynamite;
@@ -8,6 +9,58 @@ import com.inf4705.tp2.model.Dynamite;
 public class ProgDyn2DynamiteMinimizer extends BaseDynamiteMinimizer {
 	@Override
 	public List<Dynamite> minimizeDynamiteUsage(List<Dynamite> dynamites, int goal) {
+		// Step 1 : Define the matrix
+		// matrix[1...n][0...N]
+		// n : number of dynamite types
+		// N : goal
+		double [][] countMatrix = new double [dynamites.size() + 1][goal + 1];
+
+		// Sort
+		// Collections.sort(dynamites, (Dynamite d1, Dynamite d2) -> d1.getPower() - d2.getPower());
+
+		// Step 2 : Set infinity and default pivots
+		for (int i = 1; i < countMatrix.length; i++) {
+			for (int j = 0; j < countMatrix[i].length; j++) {
+				Double value = Double.POSITIVE_INFINITY;
+				if(j == 0) {
+					value = 0.0;
+				}
+				countMatrix[i][j] = value;
+			}
+		}
+
+
+		// Main loop
+		for (int i = 1; i < countMatrix.length; i++) {
+			for (int j = 1; j < countMatrix[i].length; j++) {
+				Integer dynamitePower = dynamites.get(i-1).getPower();
+
+				Double left;
+				if(dynamitePower > j) {
+					left = Double.POSITIVE_INFINITY;
+				} else {
+					left = 1 + countMatrix[i][j - dynamitePower];
+				}
+
+				Double right;
+				if(i == 1) {
+					right = Double.POSITIVE_INFINITY;
+				} else {
+					right = countMatrix[i - 1][j];
+				}
+
+				Double minValue = Math.min(left, right);
+
+				countMatrix[i][j] = minValue;
+			}
+		}
+
+		System.out.printf("\n");
+		System.out.printf("Minimum : \n");
+		System.out.print(countMatrix[dynamites.size()-1][goal]);
+		System.out.printf("\n");
+
 		return new ArrayList<>();
 	}
+
 }

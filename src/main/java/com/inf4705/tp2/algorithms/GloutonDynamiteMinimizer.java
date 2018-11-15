bb11285e14b01485272c23c4efca59d883dd730e
Unknown <felix_agagnier@hotmail.ca>
2018-11-15 16:09:05 -0500

    glouton fixed again

--- src/main/java/com/inf4705/tp2/algorithms/GloutonDynamiteMinimizer.java
+++ src/main/java/com/inf4705/tp2/algorithms/GloutonDynamiteMinimizer.java
@@ -14,16 +14,11 @@ public class GloutonDynamiteMinimizer extends BaseDynamiteMinimizer {
         dynamites.sort(Comparator.comparing(Dynamite::getPower).reversed());
         List<Dynamite> solution =  new ArrayList<>();
         int sum = 0;
-        int index = 0;
-        while (sum < goal){
-            index = max(dynamites, goal - sum, index);
-            if(index == -1){//no dynamites fits, abort
-                break;
+        for(Dynamite d : dynamites){
+            if(sum + d.getPower() <= goal){
+                solution.add(d);
+                sum += d.getPower();
             }
-            Dynamite max = dynamites.get(index);
-            index++;
-            solution.add(max);
-            sum += max.getPower();
         }
         return solution;
     }
@@ -31,17 +26,6 @@ public class GloutonDynamiteMinimizer extends BaseDynamiteMinimizer {
     @Override
     public double getFX(int goal) {
         return 1;
-    }
-
-    private int max(List<Dynamite> dynamites, int max, Integer index) {
-        while (index < dynamites.size()){
-            Dynamite dyn = dynamites.get(index);
-            if (dyn.getPower() < max) {
-                return index;
-            }
-            index++;
-        }
-        return -1;
     }
 
 }
